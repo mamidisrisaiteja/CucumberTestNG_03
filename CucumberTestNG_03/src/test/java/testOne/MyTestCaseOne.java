4d455f9fe1d73c1151e0a3fcb53483bd7a2d1544
mamidisrisaiteja@gmail.com <mamidisrisaiteja@gmail.com>
2023-12-28 00:48:55 +0530

    second commit

--- CucumberTestNG_03/src/test/java/testOne/MyTestCaseOne.java
+++ CucumberTestNG_03/src/test/java/testOne/MyTestCaseOne.java
@@ -1,11 +1,20 @@
 package testOne;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class MyTestCaseOne {
+	
+	WebDriver driver;
 	@Test
 	public void testOne() 
 	{
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.get("https://ui.cogmento.com/");
 		System.out.println("Hai in my first test");
 	}
 
